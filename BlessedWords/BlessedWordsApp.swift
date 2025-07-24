cea593e1004a73f27f55ddd50be3e42ecfed6b88
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-07-24 09:59:38 -0300

    Criação do Projeto
    Criando o projeto e fazendo configurações iniciais.

---
+++ BlessedWords/BlessedWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlessedWordsApp.swift
+//  BlessedWords
+//
+//  Created by Juliano Sgarbossa on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BlessedWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
